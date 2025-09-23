662036ddf34cb2bd9b693e27e56172287ee60f98
Ayush_Pundir <ayushpundir11@gmail.com>
2025-09-23 10:20:24 +0530

    added comments for better understanding of react hook useState

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,6 +5,14 @@ import RestaurantCard from "./RestaurantCard";
 
 const Body = () => {
     //local state variable 
+    //this listResturant is a super powerful react variable this is how we create it
+    // inside useState we give default values and we use a set{variable_name}
+    // method to update this react variable this set method triggers the diff algo
+
+    //this super powerful variable keeps the ui in sync with data layer
+    //as soon as it change it will automatically refresh our component
+
+    //whenever a state variable changes react will re-render my component
     const [listRestaurants, setlistRestaurants] = useState([...resObj]);
     return (
         <div className="body">
