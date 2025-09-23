404313400c60e8d66d3788efb62ca9722e560a59
Ayush_Pundir <ayushpundir11@gmail.com>
2025-09-23 16:20:04 +0530

    earlier I was using same image for all restaurants cards but now I'm using cloudinaryid + fixed url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/jjg0liga3l9lfyefosok";
+export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 export const LOGO_URL = "https://www.designmantic.com/logo-images/171406.png?company=Company%20Name&keyword=food&slogan=&verify=1";
