20804a6ec39b5a896b490ef09325d3e0e306bb34
Ayush_Pundir <ayushpundir11@gmail.com>
2025-12-02 22:46:41 +0530

    created higher order function done styling of existing resturant card component

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,16 +1,36 @@
 import { CDN_URL } from "../utils/constants";
 
-const RestaurantCard = ({resData}) => { // these are props which contain object of everything we pass to it prop = {0:{info:{name:burgerKing}}}
+const RestaurantCard = ({resData}) => { // these are props which contain object of everything we pass to it prop = {resData: restaurant data}
  const {name, cuisines, avgRating, cloudinaryImageId} = resData.info;
     return (
-        <div className= "res-card" id = {resData.info.id}>
-            <img className = "res-logo"
+        <div className= "m-4 p-4 w-[200px] rounded-lg bg-gray-100 hover:bg-gray-200" id = {resData.info.id}>
+            <img className = "res-logo rounded-lg"
             src = {CDN_URL+cloudinaryImageId}
             />
-            <h3>{name}</h3>
+            <h3 className="font-bold py-4 text-lg">{name}</h3>
             <h4>{cuisines.join(", ")}</h4>
             <h4>rating:{avgRating}</h4>
         </div>
     )
 };
 export default RestaurantCard;
+
+/**
+ *  
+ * creating a higer order component to show promoted restaurants
+ * withPromotedLabel is a function (higher order component) which takes RestaurantCard component as an argument
+   and returns the promoted list} RestaurantCard 
+ */
+/**
+ * it is returning another functional component which takes props as argument
+ */
+export const withPromotedLabel = (RestaurantCard) => {
+    return (props) => {
+        return (
+            <div>
+                <div className="absolute bg-black text-white px-2 py-1 rounded-bl-lg rounded-tr-lg">PROMOTED</div>
+                <RestaurantCard {...props}/>
+            </div>
+        )
+    }
+}
