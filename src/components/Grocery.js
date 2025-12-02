e58124a57e3a27012a84b24ec7824c933a4be01c
Ayush_Pundir <ayushpundir11@gmail.com>
2025-12-02 22:41:23 +0530

    built demo grocery store to learn about lazy loading

---
+++ src/components/Grocery.js
@@ -0,0 +1,6 @@
+const Grocery = () => {
+    return (
+        <h1>This is our Grocery Store, we have divided our project into 2 main section food and grocery just for practicing chunking/ code splitting</h1>
+    );
+};
+export default Grocery;
