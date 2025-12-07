df97f1166f3329d3d9eb9bb9bb3e8bea804abc87
Ayush_Pundir <ayushpundir11@gmail.com>
2025-12-07 22:48:11 +0530

    since this is a demo website, hidden phone number for privacy

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,7 @@ const Contact = () => {
     return (
         <div>
             <h1>Mail:ayushpundir11@gmail.com</h1>
-            <h1>phone:8273700549</h1>
+            <h1>phone:827XXXXX49</h1>
         </div>
     )
 }
