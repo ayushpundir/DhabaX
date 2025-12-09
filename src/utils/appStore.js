6e0747ad082f46ad793335ec1eae8f150205bf84
Ayush_Pundir <ayushpundir11@gmail.com>
2025-12-09 22:41:58 +0530

    built our redux store is just a big centralised object

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -2,6 +2,8 @@ import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./redux-slices/cartSlice";
 
 //the reducer which we have here is our appStore reducer which combines different reducers of each slice
+//difference between reduxer and reducers 
+//appStore has one big reducer and this reducer is made up of different smaller reducers from different slices
 const appStore = configureStore({
     reducer: {
         //add different reducers here
