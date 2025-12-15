ac642f2a93209da8bf89a074dbea0d208cee7ed0
Ayush_Pundir <ayushpundir11@gmail.com>
2025-12-15 22:25:04 +0530

    modified Contact Us page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,20 @@
 const Contact = () => {
     return (
         <div>
-            <h1>Mail:ayushpundir11@gmail.com</h1>
-            <h1>phone:827XXXXX49</h1>
+            <h1 className="font-bold text-3xl m-4 p-4">Contact used</h1>
+            <form>
+                <input 
+                type="text" 
+                placeholder="Name" 
+                className="border border-black m-2 p-2 rounded"
+                />
+                <input 
+                type="text" 
+                placeholder="Message" 
+                className="border border-black m-2 p-2 rounded"
+                />
+                <button className="bg-blue-500 text-white m-2 p-2 rounded">Submit</button>
+            </form>
         </div>
     )
 }
