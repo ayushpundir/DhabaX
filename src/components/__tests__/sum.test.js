da7c3ad84a6e54c17d496db66ea0338ed64125f3
Ayush_Pundir <ayushpundir11@gmail.com>
2025-12-20 20:02:44 +0530

    added comments

--- src/components/__tests__/sum.test.js
+++ src/components/__tests__/sum.test.js
@@ -1,8 +1,10 @@
 import { sum } from "../sum";
 
+//this test function takes two arguments, a string description and a callback function
+
 test("Sum function should caculate the sum of two numbers", () => {
   const result = sum(3, 4);
 
-  //Assertion
+  //Assertion, if we won't use assertion the test will always pass
   expect(result).toBe(7);
 });
