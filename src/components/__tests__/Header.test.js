920cffbc10208fa717e6cdf5b9e4a706b010b56a
Ayush_Pundir <ayushpundir11@gmail.com>
2025-12-24 00:54:53 +0530

    instead of browserrouter used MemoryRouter and made some change in header

--- src/components/__tests__/Header.test.js
+++ src/components/__tests__/Header.test.js
@@ -2,43 +2,43 @@ import Header from "../Header";
 import { fireEvent, render, screen } from "@testing-library/react";
 import appStore from "../../utils/appStore";
 import {Provider} from "react-redux";
-import { BrowserRouter } from "react-router";
+import { MemoryRouter } from "react-router";
 import "@testing-library/jest-dom";
 
 it("should render Header component with a login button", () => {
     render(
-        <BrowserRouter>
+        <MemoryRouter>
         <Provider store={appStore}>
             <Header/>
         </Provider>
-        </BrowserRouter>
+        </MemoryRouter>
     );
 
-    const loginButton = screen.getByRole("button", { name: "login" }); // sepcifying role and name, if there were multiple buttons
+    const loginButton = screen.getByRole("button", { name: "Login" }); // sepcifying role and name, if there were multiple buttons
     expect(loginButton).toBeInTheDocument();
 });
 
 it("should render Header component with 0 cart items", () => {
     render(
-        <BrowserRouter>
+        <MemoryRouter>
         <Provider store={appStore}>
             <Header/>
         </Provider>
-        </BrowserRouter>
+        </MemoryRouter>
     );
 
-    const cartItems = screen.getByText("Cart - 0 items");
-    expect(cartItems).toBeInTheDocument();
+    expect(screen.getByText("0")).toBeInTheDocument();
+
 });
 
 //should render Header component with cart irrespective of number of items in cart
 it("should render Header component with cart", () => {
     render(
-        <BrowserRouter>
+        <MemoryRouter>
         <Provider store={appStore}>
             <Header/>
         </Provider>
-        </BrowserRouter>
+        </MemoryRouter>
     );
 
     const cartItems = screen.getByText(/Cart/);// regex to match cart text irrespective of number of items
@@ -47,15 +47,15 @@ it("should render Header component with cart", () => {
 
 it("should change login button to logout on click", () => {
     render(
-        <BrowserRouter>
+        <MemoryRouter>
         <Provider store={appStore}>
             <Header/>
         </Provider>
-        </BrowserRouter>
+        </MemoryRouter>
     );
 
-    const loginButton = screen.getByRole("button", { name: "login" }); // sepcifying role and name, if there were multiple buttons
+    const loginButton = screen.getByRole("button", { name: "Login" }); // sepcifying role and name, if there were multiple buttons
     fireEvent.click(loginButton);
-    const logoutButton = screen.getByRole("button", { name: "logout" });
+    const logoutButton = screen.getByRole("button", { name: "Logout" });
     expect(logoutButton).toBeInTheDocument();
 });
